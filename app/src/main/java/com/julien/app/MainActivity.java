ebe14dc2876ed4987286ee8ac6b29b43621496a2
lim8639 <863978160@qq.com>
2021-04-25 15:54:38 +0800

    更新保存用户密码，能够使用不同账户登录

--- app/src/main/java/com/julien/app/MainActivity.java
+++ app/src/main/java/com/julien/app/MainActivity.java
@@ -2,7 +2,10 @@ package com.julien.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -40,23 +43,26 @@ public class MainActivity extends AppCompatActivity {
     EditText user = null;
     EditText pwd = null;
     EditText service = null;
+
+    String []data =new String[5];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
+    // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.planets_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+    // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-// Apply the adapter to the spinner
+     // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-        spinner.setSelection(2,true);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+               String text=(String) spinner.getSelectedItem();
+               data[2]= text;
+               data[4] = String.valueOf(position);
             }
 
             @Override
@@ -64,27 +70,44 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
         button = this.findViewById(R.id.btn);
         button2 = this.findViewById(R.id.btn_logout);
         textView = this.findViewById(R.id.text);
         user = this.findViewById(R.id.user);
         pwd = this.findViewById(R.id.pwd);
+
+        String []shareData =  getData();
+        if ("null".equals(shareData[0])){
+            spinner.setSelection(2,true);
+        }else {
+            user.setText(shareData[0]);
+            pwd.setText(shareData[1]);
+            spinner.setSelection(Integer.parseInt(shareData[4]),true);
+        }
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myHttp("201811050496", "", "中国电信");
+                data[0] = String.valueOf(user.getText());
+                data[1] = String.valueOf(pwd.getText());
+                data[2]=(String) spinner.getSelectedItem();
+                savedata(data);
+                System.out.println(data[0]+data[1]+data[2]);
+                myHttp(data[0],data[1],data[2]);
             }
         });
+
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new Thread(new Runnable() {
                     public void run() {
-                        runOnUiThread(new Runnable() {
+                        textView.post(new Runnable() {
                             @Override
                             public void run() {
-                                myHttp("201811050496", "", "中国电信");
+                                textView.setText("功能待开发");
                             }
                         });
                     }
@@ -92,11 +115,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // 保存数据
+   void  savedata(String []data){
+       //获取SharedPreferences对象
+       Context ctx = MainActivity.this;
+       SharedPreferences sp = ctx.getSharedPreferences("SP", MODE_PRIVATE);
+       //存入数据
+       SharedPreferences.Editor editor = sp.edit();
+       editor.putString("username", data[0]);
+       editor.putString("password", data[1]);
+       editor.putString("service", data[2]);
+       editor.putString("isSave",data[3]);
+       editor.putString("serviceId",data[4]);
+       editor.commit();
+       //返回account的值,如果没有就返回默认的empty
+       Log.d("SP", sp.getString("account", "empty"));
+   }
+
+   // 读取数据
+     String [] getData(){
+         Context ctx = MainActivity.this;
+         SharedPreferences sp = ctx.getSharedPreferences("SP", MODE_PRIVATE);
+         return new String[]{
+                 sp.getString("username", "null"),
+                sp.getString("password", "null"),
+                sp.getString("service", "null"),
+                sp.getString("isSave", "null"),
+                sp.getString("serviceId", "null")
+         };
+     }
+
+    /**
+     * url 编码
+     * @param url
+     * @return
+     */
     private  String encode(String url)
     {
 
         try {
-
             String encodeURL= URLEncoder.encode( url, "UTF-8" );
 
             return encodeURL;
@@ -121,13 +179,13 @@ public class MainActivity extends AppCompatActivity {
         FormBody formBody = new FormBody.Builder()
                 .add("userId", username)
                 .add("password", password)
-                .add("service", service)
+                .add("service", encodeService)
                 .add("queryString", queryString)
                 .build();
-        String params = "userId=201811050496&password=&service=%25E4%25B8%25AD%25E5%259B%25BD%25E7%2594%25B5%25E4%25BF%25A1&queryString=wlanuserip%253D57d35c239b2324316cd24c6e51f95581%2526wlanacname%253D86aeba9fa75c3397517ce58fe6aca3a1%2526ssid%253D%2526nasip%253D4826f7b7351dd62c90efb453f599d425%2526snmpagentip%253D%2526mac%253D511fa98d409af714ebee766c3186de09%2526t%253Dwireless-v2%2526url%253D2c0328164651e2b4f13b933ddf36628bea622dedcc302b30%2526apmac%253D%2526nasid%253D86aeba9fa75c3397517ce58fe6aca3a1%2526vid%253D0876c30aa757a4d0%2526port%253D5dc4946d64dba368%2526nasportid%253D5b9da5b08a53a540de3a39287dd9a647b4e8dc0881e46769a6625c1012b5c045&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false";
+        String params = "userId="+username+"&password="+username+"&service="+encodeService+"&queryString=wlanuserip%253D57d35c239b2324316cd24c6e51f95581%2526wlanacname%253D86aeba9fa75c3397517ce58fe6aca3a1%2526ssid%253D%2526nasip%253D4826f7b7351dd62c90efb453f599d425%2526snmpagentip%253D%2526mac%253D511fa98d409af714ebee766c3186de09%2526t%253Dwireless-v2%2526url%253D2c0328164651e2b4f13b933ddf36628bea622dedcc302b30%2526apmac%253D%2526nasid%253D86aeba9fa75c3397517ce58fe6aca3a1%2526vid%253D0876c30aa757a4d0%2526port%253D5dc4946d64dba368%2526nasportid%253D5b9da5b08a53a540de3a39287dd9a647b4e8dc0881e46769a6625c1012b5c045&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false";
         RequestBody body = RequestBody.create(MediaType.parse("application/x-www-form-urlencoded;charset=utf-8"),
                 params);
-        Map<String, String> h = new HashMap<>();
+        Map<String, String> h = new HashMap<String, String>();
         final Request request = new Request.Builder()
                 .url("http://auth.ysu.edu.cn/eportal/InterFace.do?method=login")  //请求的url
                 .post(body)//设置请求方式，get()/post()  查看Builder()方法知，在构建时默认设置请求方式为GET
@@ -157,12 +215,12 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
                 System.out.println("连接失败");
             }
-
             //异步请求(非主线程)
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if (response.code() == 200) {
-                    String re = response.body().string();
+                    final String re = response.body().string();
+                    System.out.println(re);
                     new Thread(new Runnable() {
                         public void run() {
                             textView.post(new Runnable() {
@@ -170,7 +228,7 @@ public class MainActivity extends AppCompatActivity {
                                 public void run() {
                                     try {
                                         JSONObject jsonObject = new JSONObject(re);
-                                      String  result = jsonObject.get("result").toString();
+                                        String  result = jsonObject.get("result").toString();
                                         textView.setText(result);
                                     } catch (JSONException e) {
                                         e.printStackTrace();
---
+++ app/src/main/java/com/julien/app/ShareUtils.java
@@ -0,0 +1,12 @@
+package com.julien.app;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * 保存账户数据
+ */
+public class ShareUtils {
+
+}
